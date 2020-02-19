0b7916f1a950d8cb586461a006884cdb99b1cea4
NelliPestych <NelliPestych@bigmir.net>
2020-02-19 21:30:08 +0200

    new com

--- homework-4/task-1.js
+++ homework-4/task-1.js
@@ -1,19 +1,27 @@
 'use strict';
 
-const user = {
-  name: 'Mango',
-  age: 20,
-  hobby: 'html',
-  premium: true,
+const account = {
+  owner: 'Mango',
+  balance: 24000,
+  discount: 0.1,
+  orders: ['order-1', 'order-2', 'order-3'],
+  changeDiscount(value) {
+    this.discount = value;
+  },
+  showOrders() {
+    return this.orders;
+  },
+  addOrder(cost, order) {
+    this.balance -= cost;
+    this.orders.push(order);
+  },
 };
-user.mood = 'happy';
-user.hobby = 'skydiving';
-user.premium = false;
 
-const entries = Object.entries(user);
-for (const entry of entries) {
-  const key = entry[0];
-  const value = entry[1];
+account.changeDiscount(0.15);
+console.log(account.discount); // 0.15
 
-  console.log(`${key}: ${value}`);
-}
+console.table(account.showOrders()); // ['order-1', 'order-2', 'order-3']
+
+account.addOrder(5000, 'order-4');
+console.log(account.balance); // 19000
+console.table(account.showOrders()); // ['order-1', 'order-2', 'order-3', 'order-4']
